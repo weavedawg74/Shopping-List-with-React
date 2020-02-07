d5cab11203d430bc1656d7653a9b1fa6086ae5e9
weavedawg74 <weavedawg74@yahoo.com>
2020-02-06 20:12:51 -0600

    worked on edit function

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import ShoppingList from './ShoppingList';
 import uuidv4 from 'uuid/v4';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import bakeryIcon from "./images/bakery_icon.png";
-import breadIcon from "./images/bread_icon.png";
 import cansIcon from "./images/cans_icon.png";
 import dairyIcon from "./images/dairy_icon.png";
 import deliIcon from "./images/deli_icon.png";
@@ -11,6 +10,10 @@ import freezerIcon from "./images/freezer_icon.png";
 import householdIcon from "./images/household_icon.png";
 import meatIcon from "./images/meat_icon.png";
 import produceIcon from "./images/produce_icon.png";
+import fishIcon from "./images/fish_icon.png";
+import beverageIcon from "./images/beverage_icon.png";
+import condimentsIcon from "./images/condiment_icon.png";
+import petIcon from "./images/pet_icon.png";
 
 
 const LOCAL_STORAGE_KEY = 'shoppingApp.itemLists'
@@ -19,13 +22,16 @@ function App() {
   //const [item, setItems] = useState([])  // we will get rid of this
   const [itemLists, setItemLists] = useState({
     'Bakery': [],
-    'Bread': [],
+    'Beverage': [],
     'Cans': [],
+    'Condiments': [],
     'Dairy': [],
     'Deli': [],
+    'Fish': [],
     'Frozen': [],
     'Household': [],
     'Meat': [],
+    'Pet': [],
     'Produce': [],
   })
   const itemNameRef = useRef()
@@ -52,24 +58,13 @@ function App() {
     })
   }
 
-  /*function toggleItem(id) {
-    const newItems = [...item]
-    const items = newItems.find(item => item.id === id)
-    items.got = !items.got
-    setItems(newItems)
-  }
-  */
   function deleteItem(id, listName) {
-
     setItemLists( prevItems => {
-      // Step 1: get the WHOLE data structure from the state hook getter and clone it 
       const copyPrevItems = {...prevItems}
-      // Step 2: modify the (shallow) copied data structure
       const currentList = copyPrevItems[listName]
       const itemIndex = currentList.findIndex(item => item.id === id)
       currentList.splice(itemIndex, 1)
-      // Step 3: return the modified data structure
-      return copyPrevItems;
+      return copyPrevItems
     })
   }
 
@@ -81,18 +76,16 @@ function App() {
       // Step 2: modify the (shallow) copied data structure
       const currentList = copyPrevItems[listName]
       const itemIndex = currentList.findIndex(item => item.id === id)
-      currentList[itemIndex].isEdited = !currentList[itemIndex].isEdited;
+      currentList[itemIndex].isEdited = !currentList[itemIndex].isEdited
       // Step 3: If you finished editing, then change the name
       // (when the item is not edited anymore --> isEdited property is changed to false)
       if ( !currentList[itemIndex].isEdited ) {
         currentList[itemIndex].itemName = newItemName
       }
       // Step 4: return the modified data structure
-      return copyPrevItems;
+      return copyPrevItems
     })
   }
-
-
   /*
     TODO for making Edit work
       1. In Item.js, render a textfield when you see EDITING
@@ -100,27 +93,18 @@ function App() {
   */
 
   function handleAddItem(event) {
-    event.preventDefault();
-
+    event.preventDefault()
     const itemName = itemNameRef.current.value
     const itemListName = itemListRef.current.value
     if (itemName === '') return 
-
     setItemLists( prevItems => {
-      const copyPrevItems = {...prevItems};
-      const newValue = {id: uuidv4(), itemName: itemName, complete: false, isEdited: false};
-      copyPrevItems[itemListName].push( newValue );
-      return copyPrevItems;
+      const copyPrevItems = {...prevItems}
+      const newValue = {id: uuidv4(), itemName: itemName, complete: false, isEdited: false}
+      copyPrevItems[itemListName].push( newValue )
+      return copyPrevItems
     })
-
-    // DELETE THIS LATER ALONG WITH THE setItems useState hook
-    /*
-    setItems(prevItems => {
-      return [...prevItems, {id: uuidv4(), itemName: itemName, complete: false}]
-    }) */
-
     itemNameRef.current.value = null
-    itemListRef.current.value = ""  // Set it to a proper value
+    itemListRef.current.value = ""
   }
 
   return (
@@ -178,13 +162,16 @@ function App() {
             <select className="browser-default custom-select" id="selectedOption" ref={itemListRef}>
               <option value="" disabled>Select your option</option>
               <option value="Bakery"> Bakery</option>
-              <option value="Bread"> Bread</option>
+              <option value="Beverage"> Beverage</option>
               <option value="Cans"> Cans</option>
+              <option value="Condiments"> Condiments</option>
               <option value="Dairy"> Dairy</option>
               <option value="Deli"> Deli</option>
+              <option value="Fish"> Fish</option>
               <option value="Frozen"> Frozen</option>
               <option value="Household"> Household</option>
               <option value="Meat"> Meat</option>
+              <option value="Pet"> Pet</option>
               <option value="Produce"> Produce </option>
             </select>
           </div>
@@ -211,25 +198,37 @@ function App() {
             Bakery      
           </div>
           <div className="card-body" id="bakeryList" title="Bakery">
-            <ShoppingList item={itemLists['Bakery']} toggleItem={id => toggleItem(id, 'Bakery')} deleteItem={id => deleteItem(id, 'Bakery')} editItem={(id, name) => toggleEditItem(id, name, 'Bakery')} />
+            <ShoppingList item={itemLists['Bakery']} toggleItem={id => toggleItem(id, 'Bakery')} deleteItem={id => deleteItem(id, 'Bakery')} editItem={(id, name) => toggleEditItem(id, name, 'Bakery')}/>
           </div>
         </div>
+
         <div className="card">
-          <div className="card-header bg-info text-white" id="bread">
-            <img src={breadIcon} className="list-icon" alt="clipart of bread" style={iconStyle}></img>
-            Bread
+          <div className="card-header bg-info text-white" id="beverage">
+            <img src={beverageIcon} className="list-icon" alt="clipart of a drink" style={iconStyle}></img>
+            Beverage      
           </div>
-          <div className="card-body" id="breadList">
-          <ShoppingList item={itemLists['Bread']} toggleItem={toggleItem} deleteItem={id => deleteItem(id, 'Bread')} editItem={(id, name) => toggleEditItem(id, name, 'Bread')} />
+          <div className="card-body" id="beverageList">
+            <ShoppingList item={itemLists['Beverage']} toggleItem={id => toggleItem(id, 'Beverage')} deleteItem={id => deleteItem(id, 'Beverage')} editItem={(id, name) => toggleEditItem(id, name, 'Beverage')} />
           </div>
         </div>
+
         <div className="card">
           <div className="card-header bg-info text-white" id="cannedgood">
             <img src={cansIcon} className="list-icon" alt="clipart of canned good" style={iconStyle}></img>
             Cans
           </div>
           <div className="card-body" id="cannedgoodList">
-            <ShoppingList item={itemLists['Cans']} toggleItem={toggleItem} deleteItem={id => deleteItem(id, 'Cans')} editItem={(id, name) => toggleEditItem(id, name, 'Cans')} />
+            <ShoppingList item={itemLists['Cans']} toggleItem={id => toggleItem(id, 'Cans')} deleteItem={id => deleteItem(id, 'Cans')} editItem={(id, name) => toggleEditItem(id, name, 'Cans')} />
+          </div>
+        </div>
+
+        <div className="card">
+          <div className="card-header bg-info text-white" id="condiments">
+            <img src={condimentsIcon} className="list-icon" alt="clipart of ketchup and mustard" style={iconStyle}></img>
+            Condiments      
+          </div>
+          <div className="card-body" id="condimentsList">
+            <ShoppingList item={itemLists['Condiments']} toggleItem={id => toggleItem(id, 'Condiments')} deleteItem={id => deleteItem(id, 'Condiments')} editItem={(id, name) => toggleEditItem(id, name, 'Condiments')} />
           </div>
         </div>
 
@@ -239,52 +238,77 @@ function App() {
             Dairy
           </div>
           <div className="card-body" id="dairyList">
-          <ShoppingList item={itemLists['Dairy']} toggleItem={toggleItem} deleteItem={id => deleteItem(id, 'Dairy')} editItem={(id, name) => toggleEditItem(id, name, 'Dairy')} />
+          <ShoppingList item={itemLists['Dairy']} toggleItem={id => toggleItem(id, 'Dairy')} deleteItem={id => deleteItem(id, 'Dairy')} editItem={(id, name) => toggleEditItem(id, name, 'Dairy')} />
           </div>
         </div>
+
         <div className="card">
           <div className="card-header bg-info text-white" id="deli">
             <img src={deliIcon} className="list-icon" alt="clipart of sandwich" style={iconStyle}></img>
             Deli
           </div>
           <div className="card-body" id="deliList">
-          <ShoppingList item={itemLists['Deli']} toggleItem={toggleItem} deleteItem={id => deleteItem(id, 'Deli')} editItem={(id, name) => toggleEditItem(id, name, 'Deli')} />
+          <ShoppingList item={itemLists['Deli']} toggleItem={id => toggleItem(id, 'Deli')} deleteItem={id => deleteItem(id, 'Deli')} editItem={(id, name) => toggleEditItem(id, name, 'Deli')} />
           </div>
         </div>
+
+        <div className="card">
+          <div className="card-header bg-info text-white" id="fish">
+            <img src={fishIcon} className="list-icon" alt="clipart of fish" style={iconStyle}></img>
+            Fish      
+          </div>
+          <div className="card-body" id="fishList">
+            <ShoppingList item={itemLists['Fish']} toggleItem={id => toggleItem(id, 'Fish')} deleteItem={id => deleteItem(id, 'Fish')} editItem={(id, name) => toggleEditItem(id, name, 'Fish')} />
+          </div>   
+        </div>
+
         <div className="card">
           <div className="card-header bg-info text-white" id="frozen">
             <img src={freezerIcon} className="list-icon" alt="clipart of snowflake" style={iconStyle}></img>
             Frozen
           </div>
           <div className="card-body" id="frozenList">
-            <ShoppingList item={itemLists['Frozen']} toggleItem={toggleItem} deleteItem={id => deleteItem(id, 'Frozen')} editItem={(id, name) => toggleEditItem(id, name, 'Frozen')} />
+            <ShoppingList item={itemLists['Frozen']} toggleItem={id => toggleItem(id, 'Frozen')} deleteItem={id => deleteItem(id, 'Frozen')} editItem={(id, name) => toggleEditItem(id, name, 'Frozen')} />
           </div>
         </div>
+
         <div className="card">
           <div className="card-header bg-info text-white" id="household">
             <img src={householdIcon} className="list-icon" alt="clipart of cleaning items" style={iconStyle}></img>
             Household
           </div>
           <div className="card-body" id="householdList">
-            <ShoppingList item={itemLists['Household']} toggleItem={toggleItem} deleteItem={id => deleteItem(id, 'Household')} editItem={(id, name) => toggleEditItem(id, name, 'Household')} />
+            <ShoppingList item={itemLists['Household']} toggleItem={id => toggleItem(id, 'Household')} deleteItem={id => deleteItem(id, 'Household')} editItem={(id, name) => toggleEditItem(id, name, 'Household')} />
           </div>
         </div>
+
         <div className="card">
           <div className="card-header bg-info text-white" id="meat">
             <img src={meatIcon} className="list-icon" alt="clipart of steak" style={iconStyle}></img>
             Meat
           </div>
           <div className="card-body" id="meatList">
-          <ShoppingList item={itemLists['Meat']} toggleItem={toggleItem} deleteItem={id => deleteItem(id, 'Meat')} editItem={(id, name) => toggleEditItem(id, name, 'Meat')} />
+          <ShoppingList item={itemLists['Meat']} toggleItem={id => toggleItem(id, 'Meat')} deleteItem={id => deleteItem(id, 'Meat')} editItem={(id, name) => toggleEditItem(id, name, 'Meat')} />
           </div>
         </div>
+
+        <div className="card">
+          <div className="card-header bg-info text-white" id="pet">
+            <img src={petIcon} className="list-icon" alt="clipart of dog and cat" style={iconStyle}></img>
+            Pet      
+          </div>
+          <div className="card-body" id="petList">
+            <ShoppingList item={itemLists['Pet']} toggleItem={id => toggleItem(id, 'Pet')} deleteItem={id => deleteItem(id, 'Pet')} editItem={(id, name) => toggleEditItem(id, name, 'Pet')} />
+          </div>
+        </div>
+
         <div className="card">
           <div className="card-header bg-info text-white" id="produce">
             <img src={produceIcon} className="list-icon" alt="clipart of broccoli" style={iconStyle}></img>
             Produce      
           </div>
           <div className="card-body" id="produceList">
-          <ShoppingList item={itemLists['Produce']} toggleItem={toggleItem} deleteItem={id => deleteItem(id, 'Produce')} editItem={(id, name) => toggleEditItem(id, name, 'Produce')} />
+          <ShoppingList item={itemLists['Produce']} toggleItem={id => toggleItem(id, 'Produce')} deleteItem={id => deleteItem(id, 'Produce')} editItem={(id, name) => toggleEditItem(id, name, 'Produce')} />
           </div>
         </div>
       </div>
--- src/Item.js
+++ src/Item.js
@@ -1,11 +1,14 @@
-import React from 'react'
+import React, { useRef, } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEdit } from '@fortawesome/free-solid-svg-icons'
 import { faTrash } from '@fortawesome/free-solid-svg-icons'
-//import { faSquare } from '@fortawesome/free-solid-svg-icons'
-//import { faCheckSquare } from '@fortawesome/free-solid-svg-icons'
+import { faCheck } from '@fortawesome/free-solid-svg-icons'
+import { faTimes} from '@fortawesome/free-solid-svg-icons'
 
-export default function Item( {item, toggleItem, deleteItem, editItem, isEdited, isGot} ) {
+
+export default function Item( {item, toggleItem, deleteItem, editItem, isEdited, isGot, addEditedItem} ) {
+    const editItemRef = useRef()
+
     function handleItemClick() {
         toggleItem(item.id, 'YOU GOT IT')
     }
@@ -13,20 +16,29 @@ export default function Item( {item, toggleItem, deleteItem, editItem, isEdited,
         deleteItem(item.id)
       }  
     function handleItemEdit() {
-        editItem(item.id, 'DUMMY CHANGED VALUE')
+        //const editItemValue = editItemRef.current.value
+        editItem(item.id, 'edited')
     }  
+    //function handleAddEditedItem() {
+    //    const editItemValue = editItemRef.current.value
+    //    addEditedItem(item.id, {editItemValue})
+    //}
+
+    
     
     const checkbox = <input type="checkbox" checked={item.got} onChange={handleItemClick} />
-    const isEditedFlagTEMPORARY = isEdited ? "EDITING" : ""
-    const itemGot = isGot ? "GOT" : ""
-    const itemName = <span className = "mr-auto p-2">{item.itemName}</span>
-    const editBtn = <i className='fa fa-edit' onClick={handleItemEdit}><FontAwesomeIcon icon={faEdit} className = "text-primary p2 mx-2"/></i>
-    const deleteBtn = <i className='fa fa-trash' onClick={handleItemDelete}><FontAwesomeIcon icon={faTrash} className="text-danger p2 mx-2"/></i>
-    const itemToAdd = (<p className ="listItem d-inline-flex align-items-center flex-wrap w-100">{checkbox}{itemGot}{itemName}{editBtn}{isEditedFlagTEMPORARY}{deleteBtn}</p>
-    )
+    const isEditedFlagTEMPORARY = isEdited ? " " : ""
+    const checkBtn = <i className='fa fa-check' hidden={!isEditedFlagTEMPORARY}><FontAwesomeIcon icon={faCheck} className = "text-success p2 mx-2"/></i>
+    const cancelBtn = <i className='fa fa-times' onClick={handleItemEdit} hidden={!isEditedFlagTEMPORARY}><FontAwesomeIcon icon={faTimes} className = "text-danger p2 mx-2"/></i>
+    const editField = <input type="text" placeholder={item.itemName} ref={editItemRef} hidden={!isEditedFlagTEMPORARY} />
+    const itemGot = isGot ? <strike>{item.itemName}</strike> : ""
+    const itemName = <span className = "mr-auto p-2" hidden={isEditedFlagTEMPORARY || isGot}>{item.itemName}</span>
+    const editBtn = <i className='fa fa-edit' hidden={isEditedFlagTEMPORARY} onClick={handleItemEdit}><FontAwesomeIcon icon={faEdit} className = "text-primary p2 mx-2"/></i>
+    const deleteBtn = <i className='fa fa-trash' onClick={handleItemDelete} hidden={isEditedFlagTEMPORARY}><FontAwesomeIcon icon={faTrash} className="text-danger p2 mx-2"/></i>
+    const itemToAdd = (<p className ="listItem d-inline-flex align-items-center flex-wrap w-100">{checkbox}{itemGot}{itemName}{editField}{checkBtn}{cancelBtn}{editBtn}{deleteBtn}</p>)
     return (
         <div>
-            {itemToAdd}            
+            {itemToAdd}         
         </div>
     )
 }
