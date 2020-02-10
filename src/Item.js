d97340435a234e7b561c9a2ae6dbf7d36a01ceb1
weavedawg74 <weavedawg74@yahoo.com>
2020-02-10 06:06:11 -0600

    worked on edit

--- src/Item.js
+++ src/Item.js
@@ -1,4 +1,4 @@
-import React, { useRef, } from 'react'
+import React, { useRef } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEdit } from '@fortawesome/free-solid-svg-icons'
 import { faTrash } from '@fortawesome/free-solid-svg-icons'
@@ -6,8 +6,9 @@ import { faCheck } from '@fortawesome/free-solid-svg-icons'
 import { faTimes} from '@fortawesome/free-solid-svg-icons'
 
 
-export default function Item( {item, toggleItem, deleteItem, editItem, isEdited, isGot, addEditedItem} ) {
-    const editItemRef = useRef()
+export default function Item( {item, toggleItem, deleteItem, editItem, isEdited, isGot} ) {
+    const editedItemRef = useRef()
+
 
     function handleItemClick() {
         toggleItem(item.id, 'YOU GOT IT')
@@ -16,25 +17,29 @@ export default function Item( {item, toggleItem, deleteItem, editItem, isEdited,
         deleteItem(item.id)
       }  
     function handleItemEdit() {
-        //const editItemValue = editItemRef.current.value
-        editItem(item.id, 'edited')
+        editItem(item.id)
     }  
-    //function handleAddEditedItem() {
-    //    const editItemValue = editItemRef.current.value
-    //    addEditedItem(item.id, {editItemValue})
-    //}
-
+    function handleItemEditCancel() {
+    //    let uneditedItemName = uneditedItemRef.current.value
+    //    console.log(uneditedItemName)
+        editItem(item.id, 'Not Fucking Edited')
+    }  
+    function handleItemEditSubmit() {
+        let editedItemName = editedItemRef.current.value
+        editItem(item.id, editedItemName)
+    }
+   
     
-    
-    const checkbox = <input type="checkbox" checked={item.got} onChange={handleItemClick} />
     const isEditedFlagTEMPORARY = isEdited ? " " : ""
-    const checkBtn = <i className='fa fa-check' hidden={!isEditedFlagTEMPORARY}><FontAwesomeIcon icon={faCheck} className = "text-success p2 mx-2"/></i>
-    const cancelBtn = <i className='fa fa-times' onClick={handleItemEdit} hidden={!isEditedFlagTEMPORARY}><FontAwesomeIcon icon={faTimes} className = "text-danger p2 mx-2"/></i>
-    const editField = <input type="text" placeholder={item.itemName} ref={editItemRef} hidden={!isEditedFlagTEMPORARY} />
+    const checkbox = <input type="checkbox" checked={item.got} onChange={handleItemClick} />
+    const checkBtn = <i className='fa fa-check' onClick={handleItemEditSubmit}hidden={!isEditedFlagTEMPORARY}><FontAwesomeIcon icon={faCheck} className = "text-success p2 mx-2"/></i>
+    const cancelBtn = <i className='fa fa-times' onClick={handleItemEditCancel} hidden={!isEditedFlagTEMPORARY}><FontAwesomeIcon icon={faTimes} className = "text-danger p2 mx-2"/></i>
+    const editBtn = <i className='fa fa-edit' onClick={handleItemEdit} hidden={isEditedFlagTEMPORARY}><FontAwesomeIcon icon={faEdit} className = "text-primary p2 mx-2"/></i>
+    const deleteBtn = <i className='fa fa-trash' onClick={handleItemDelete} hidden={isEditedFlagTEMPORARY}><FontAwesomeIcon icon={faTrash} className="text-danger p2 mx-2"/></i>
+    const editField = <input type="text" className="form-control" placeholder={item.itemName} defaultValue={item.itemName} ref={editedItemRef} hidden={!isEditedFlagTEMPORARY}></input>
     const itemGot = isGot ? <strike>{item.itemName}</strike> : ""
     const itemName = <span className = "mr-auto p-2" hidden={isEditedFlagTEMPORARY || isGot}>{item.itemName}</span>
-    const editBtn = <i className='fa fa-edit' hidden={isEditedFlagTEMPORARY} onClick={handleItemEdit}><FontAwesomeIcon icon={faEdit} className = "text-primary p2 mx-2"/></i>
-    const deleteBtn = <i className='fa fa-trash' onClick={handleItemDelete} hidden={isEditedFlagTEMPORARY}><FontAwesomeIcon icon={faTrash} className="text-danger p2 mx-2"/></i>
+
     const itemToAdd = (<p className ="listItem d-inline-flex align-items-center flex-wrap w-100">{checkbox}{itemGot}{itemName}{editField}{checkBtn}{cancelBtn}{editBtn}{deleteBtn}</p>)
     return (
         <div>
