382a01f761f544fe46c2c18e32a8b162d595ecfe
weavedawg74 <weavedawg74@yahoo.com>
2020-02-13 21:09:46 -0600

    Updates

---
+++ precache-manifest.6324bc17d62b6d41a1d4b9cb7b10a2da.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "baafd8f901804074b27c55f63bd9bfac",
+    "url": "/Shopping-List-with-React/index.html"
+  },
+  {
+    "revision": "bdc8a9b32a49b6fcdb55",
+    "url": "/Shopping-List-with-React/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "fc5ce412d45733af88fe",
+    "url": "/Shopping-List-with-React/static/css/main.ec252399.chunk.css"
+  },
+  {
+    "revision": "bdc8a9b32a49b6fcdb55",
+    "url": "/Shopping-List-with-React/static/js/2.29ec9e91.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/Shopping-List-with-React/static/js/2.29ec9e91.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fc5ce412d45733af88fe",
+    "url": "/Shopping-List-with-React/static/js/main.85c2a5fa.chunk.js"
+  },
+  {
+    "revision": "4e638f8cfd23f9de1cf4",
+    "url": "/Shopping-List-with-React/static/js/runtime-main.467ab93d.js"
+  },
+  {
+    "revision": "7338f94dd9d08f653ec6fcb94739ad74",
+    "url": "/Shopping-List-with-React/static/media/bakery_icon.7338f94d.png"
+  },
+  {
+    "revision": "993d7df1fa41335a779d86c0ad1f4551",
+    "url": "/Shopping-List-with-React/static/media/beverage_icon.993d7df1.png"
+  },
+  {
+    "revision": "6bf0a98082c87977bec8c6ec846eacbb",
+    "url": "/Shopping-List-with-React/static/media/cans_icon.6bf0a980.png"
+  },
+  {
+    "revision": "ced2e1a6a8597cf62c68ff16b34612a2",
+    "url": "/Shopping-List-with-React/static/media/dairy_icon.ced2e1a6.png"
+  },
+  {
+    "revision": "c40b87001dc9ae4ad9248e19ebd608e1",
+    "url": "/Shopping-List-with-React/static/media/deli_icon.c40b8700.png"
+  },
+  {
+    "revision": "d976f913b24e6d96453e5719b7cb0da1",
+    "url": "/Shopping-List-with-React/static/media/household_icon.d976f913.png"
+  },
+  {
+    "revision": "cc7c831d9ab6d871b43645a4793eb5d3",
+    "url": "/Shopping-List-with-React/static/media/hygiene_icon.cc7c831d.png"
+  },
+  {
+    "revision": "bea0eda5b330e9acb1381ec20ec68ba3",
+    "url": "/Shopping-List-with-React/static/media/meat_icon.bea0eda5.png"
+  },
+  {
+    "revision": "f7d169d4e801181725f8ad6073ba064e",
+    "url": "/Shopping-List-with-React/static/media/pet_icon.f7d169d4.png"
+  },
+  {
+    "revision": "e6b3cf5d06cb6b2e65fc6fbc93a5f2e3",
+    "url": "/Shopping-List-with-React/static/media/produce_icon.e6b3cf5d.png"
+  },
+  {
+    "revision": "3f02e6ed83b1cdd770e54345ce45e22e",
+    "url": "/Shopping-List-with-React/static/media/shopping_cart.3f02e6ed.png"
+  }
+]);
